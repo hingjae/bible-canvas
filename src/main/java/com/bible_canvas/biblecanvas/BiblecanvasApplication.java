4aebb8616e9fb99698f77d334169ee12892557bd
hingjae <ljh23270@naver.com>
2024-12-15 23:26:37 +0900

    init

---
+++ src/main/java/com/bible_canvas/biblecanvas/BiblecanvasApplication.java
@@ -0,0 +1,13 @@
+package com.bible_canvas.biblecanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblecanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblecanvasApplication.class, args);
+	}
+
+}
