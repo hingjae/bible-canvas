59b8cd5e7f9719f02fec51cb334a918a4d6210f6
hingjae <ljh23270@naver.com>
2024-12-20 01:37:48 +0900

    home

---
+++ src/main/java/com/bible_canvas/biblecanvas/HomeController.java
@@ -0,0 +1,12 @@
+package com.bible_canvas.biblecanvas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
